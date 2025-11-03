b1c9fd4069d636648959b53f3a60d8fc8873cf38
Linus Fricke <github@linus-fricke.de>
2025-11-03 22:09:46 +0100

    Fix aes cbc encrypt for plaintext/ad with arbitrary lengths

--- src/utils/aes.rs
+++ src/utils/aes.rs
@@ -1,5 +1,3 @@
-use std::iter;
-
 use anyhow::{Result, anyhow};
 use openssl::{symm::{Cipher, Crypter, Mode}};
 
@@ -12,10 +10,8 @@ pub struct AesGcmResult {
     pub h: [u8; 16]
 }
 
-pub fn gcm_encrypt<M: ReducePoly>(nonce: &[u8; 12], key: &[u8; 16], mut plaintext: Vec<u8>, mut ad: Vec<u8>) -> Result<AesGcmResult> {
+pub fn gcm_encrypt<M: ReducePoly>(nonce: &[u8; 12], key: &[u8; 16], plaintext: Vec<u8>, ad: Vec<u8>) -> Result<AesGcmResult> {
     let mut encrypter = Crypter::new(Cipher::aes_128_ecb(), Mode::Encrypt, key, None).map_err(|err| anyhow!(err.to_string()))?;
-    let ad_len = (ad.len() as u64) * 8;
-    let plain_len = (plaintext.len() as u64) * 8;
     encrypter.pad(false);
     let mut encrypter_buffer = [0u8; 32];
 
@@ -47,18 +43,28 @@ pub fn gcm_encrypt<M: ReducePoly>(nonce: &[u8; 12], key: &[u8; 16], mut plaintex
     let mut auth_tag = GF2m::<M>::zero();
 
     // Consume additional data before encryption
-    pad_vec(&mut ad);
-    for i in 0..(ad.len() / 16) {
-        let block = get_16_bytes(&ad[i..(i+16)])?;
+    let ad_len = ad.len();
+    let ad_full_blocks = ad_len / 16;
+    let ad_block_offset = ad_len % 16;
+    for i in 0..ad_full_blocks {
+        let block = get_16_bytes(&ad[i*16..(i*16+16)])?;
         auth_tag = GF2m::<M>::new(auth_tag.value ^ u128::from_be_bytes(block));
         auth_tag = &auth_tag * &auth_key_gf;
     }
+    if ad_block_offset > 0 {
+        let mut tmp = [0u8; 16];
+        for i in 0..ad_block_offset {
+            tmp[i] = ad[ad_full_blocks*16+i];
+        }
+        auth_tag = GF2m::<M>::new(auth_tag.value ^ u128::from_be_bytes(tmp));
+        auth_tag = &auth_tag * &auth_key_gf;
+    }
 
-    let cipher_offset = plaintext.len() % 16;
-    let ciphertext_len = if cipher_offset == 0 { plaintext.len() } else  { plaintext.len() + (16 - cipher_offset) };
-    pad_vec(&mut plaintext);
+    let plain_len = plaintext.len();
+    let plain_full_blocks = plain_len / 16;
+    let plain_block_offset = plain_len % 16;
     let mut ciphertext: Vec<u8> = Vec::new();
-    for i in 0..(ciphertext_len / 16) {
+    for i in 0..plain_full_blocks {
         // Initialized at 1, need to increase before each ciphertext block
         ctr += 1;
         // set counter bytes
@@ -78,21 +84,34 @@ pub fn gcm_encrypt<M: ReducePoly>(nonce: &[u8; 12], key: &[u8; 16], mut plaintex
         auth_tag = &GF2m::<M>::new(cipher_block ^ auth_tag.value) * &auth_key_gf;
     }
 
+    if plain_block_offset > 0 {
+        ctr += 1;
+        y[12..].copy_from_slice(&ctr.to_be_bytes());
+        let block_encrypt_result = encrypter.update(&y, &mut encrypter_buffer)
+            .map_err(|err| anyhow!(err.to_string()))?;
+        if block_encrypt_result != 16 {
+            return Err(anyhow!(format!("Offset Ciphertext block did not encrypt 16 bytes")))
+        }
+        let block: [u8; 16] = get_16_bytes(&encrypter_buffer)?;
+        let mut tmp = [0u8; 16];
+
+        // only want to XOR with set bytes and keep 0-bytes for GHASH, therefore for loop
+        for i in 0..plain_block_offset {
+            tmp[i] = plaintext[plain_full_blocks*16+i] ^ block[i]
+        }
+        // only ciphertext bytes
+        ciphertext.extend_from_slice(&tmp[..plain_block_offset]);
+
+        auth_tag = GF2m::<M>::new(auth_tag.value ^ u128::from_be_bytes(tmp));
+        auth_tag = &auth_tag * &auth_key_gf;
+    }
+
     // len(A)*8 || len(ciphertext)*8 for bit lengths
-    let l = (ad_len as u128) << 64 | plain_len as u128;
+    let l = (ad_len as u128 * 8) << 64 | (plain_len as u128 * 8);
     auth_tag = &GF2m::<M>::new(l ^ auth_tag.value) * &auth_key_gf;
 
 
     return Ok(AesGcmResult { ciphertext: ciphertext, tag: (auth_tag.value ^ auth_tag_xor).to_be_bytes(), l: l.to_be_bytes(), h: auth_key});
-}
-
-fn pad_vec(input: &mut Vec<u8>) {
-    let offset = input.len() % 16;
-    if offset == 0 {
-        return;
-    }
-    let pad_len = 16 - offset;
-    input.extend(iter::repeat(0u8).take(pad_len));
 }
 
 fn get_16_bytes(arr: &[u8]) -> Result<[u8; 16]> {
