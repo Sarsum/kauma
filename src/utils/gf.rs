cef22c171544d1cd8bdb911568e1a91268bc0380
Linus Fricke <github@linus-fricke.de>
2025-11-07 22:28:38 +0100

    implement slightly faster implementation for squaring

--- src/utils/gf.rs
+++ src/utils/gf.rs
@@ -1,5 +1,5 @@
 use core::ops::{Add, Mul, Div};
-use std::{ops::{AddAssign}};
+use std::{ops::AddAssign};
 
 use num::{BigInt, One, Zero, bigint::Sign};
 
@@ -75,8 +75,78 @@ impl <M: ReducePoly> GF2m<M> {
         return Self { value: result.value, _m: Default::default() }
     }
 
+    fn spread32(x: u32) -> u64 {
+        // feature saves about 0.070 seconds on 10.000 testcases
+        if is_x86_feature_detected!("bmi2") {
+            unsafe {
+                // Keep bits masked with 1: 101010101010......1010
+                return core::arch::x86_64::_pdep_u64(x as u64, 0xAAAA_AAAA_AAAA_AAAA)
+            }
+        }
+        // fallback for chips without BMI2 (older / other architecture)
+        let mut x = (x as u64) << 32;
+        x = (x | (x >> 16)) & 0xFFFF_0000_FFFF_0000;
+        x = (x | (x >> 8))  & 0xFF00_FF00_FF00_FF00;
+        x = (x | (x >> 4))  & 0xF0F0_F0F0_F0F0_F0F0;
+        x = (x | (x >> 2))  & 0xCCCC_CCCC_CCCC_CCCC;
+        x = (x | (x >> 1))  & 0xAAAA_AAAA_AAAA_AAAA;
+        x
+    }
+
+    fn interlace_zeros_msb_u128(input: u128) -> (u128, u128) {
+        // split num into 4 32-bit blocks due to CPU feature supporting 32bit to 64bit block
+        let w0 = (input >> 96) as u32; // x^0 ... x^31
+        let w1 = (input >> 64) as u32; // x^32 ... x^63
+        let w2 = (input >> 32) as u32; // x^64 ... x^95
+        let w3 = (input) as u32;       // x^96 ... x^127
+
+        let o0 = Self::spread32(w0); // x^0 ... x^63
+        let o1 = Self::spread32(w1); // x^64 ... x^127
+        let o2 = Self::spread32(w2); // x^128 ... x^191
+        let o3 = Self::spread32(w3); // x^192 ... x^255
+
+        // merge blocks into hi and lo, where hi = x^0 .. x^127 and lo = x^128 ... x^255
+        let hi = ((o0 as u128) << 64) | (o1 as u128);
+        let lo = ((o2 as u128) << 64) | (o3 as u128);
+
+        (hi, lo)
+    }
+
+    fn reduce_256(mut hi: u128, mut lo: u128) -> u128 {
+        while lo != 0 {
+            let highest_bit = lo.trailing_zeros();
+            let bit_pos = 127 - highest_bit;
+            
+            // reduce high for the bit set in low
+            hi ^= M::MOD >> bit_pos;
+
+            // x^128 is set, we reduced it above already
+            // would create a loop when not checked
+            if bit_pos != 0 {
+                // put overflowing reduce poly bits into low
+                lo ^= M::MOD << (highest_bit + 1);
+            }
+            // clear bit we just reduced
+            lo ^= 1u128 << highest_bit;
+        }
+        hi
+    }
+
+    pub fn square_fast(self) -> Self {
+        // square by putting a zero after each bit going from MSB to LSB
+        let (hi, lo) = Self::interlace_zeros_msb_u128(self.value);
+
+        // reduce the 256 bits back into 128 for our polynomial form
+        let reduced = Self::reduce_256(hi, lo);
+        Self::new(reduced)
+    }
+
+
     pub fn square(self) -> Self {
-        Self::new(self.value) * self
+        //Self::new(self.value) * self
+        // performance is SLIGHTLY better, talking again about 0.05 - 0.07 seconds at best for 10.000 cases
+        // was hell of a rabbit hole to get the reduce working, keeping it as an easter egg
+        self.square_fast()
     }
 
     pub fn pow(mut self, mut exp: BigInt) -> Self {
@@ -86,7 +156,7 @@ impl <M: ReducePoly> GF2m<M> {
             return self.pow(-exp).inv();
         }
         let mut acc = Self::one();
-        while &exp != &BigInt::zero() {
+        while exp != BigInt::zero() {
             if &exp & BigInt::one() != BigInt::zero() {
                 acc = acc.mul(Self::new(self.value))
             }
@@ -112,7 +182,7 @@ impl <M: ReducePoly> GF2m<M> {
         // sqrt(a) = a^(2^(m-1))
         // self.pow(BigInt::one() << (M::DEG - 1))
         let mut x = Self::new(self.value);
-        for _ in 0..127 {
+        for _ in 0..(M::DEG - 1) {
             x = x.square();
         }
         x
