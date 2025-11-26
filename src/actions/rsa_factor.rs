61ea665c4ef76771723a5fdc01752ec9a318daad
Linus Fricke <github@linus-fricke.de>
2025-11-26 17:15:15 +0100

    switch to heap representation for product tree, reducing runtime a bit

--- src/actions/rsa_factor.rs
+++ src/actions/rsa_factor.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 
-use anyhow::{Result, anyhow};
-use rug::Integer;
+use anyhow::Result;
+use rug::{Assign, Integer};
 use serde::Serialize;
 use serde_json::{Value, json};
 
@@ -34,34 +34,39 @@ fn to_factored_modul(a: Integer, b: Integer) -> FactoredModul {
 
 fn gernstyle_batch_gcd(moduli: &[Integer]) -> Result<Vec<FactoredModul>> {
     // product tree for N_i
-    let prod_tree_n = product_tree(moduli)?;
+    let prod_tree_n = ProductTree::build(moduli);
     // prod_tree returns error which we are propagating if it is empty
     // hence, unwrap is safe
-    let p = prod_tree_n.last().unwrap()[0].clone();
+    let p = prod_tree_n.get_root();
 
     // product tree for Ni_squared
     let sq: Vec<Integer> = moduli.iter().map(|n| n.clone()*n).collect();
-    let prod_tree_sq = product_tree(&sq)?;
+    let prod_tree_sq = ProductTree::build(&sq);
     // safe again
-    let m = prod_tree_sq.last().unwrap()[0].clone();
+    let m = prod_tree_sq.get_root();
 
-    let root_remainder = p.clone() % &m;
+    let root_remainder = p.clone() % m;
 
     // remainder tree, pushing root remainder to the top
-    let zi = remainder_tree(&prod_tree_sq, root_remainder);
+    let zi = prod_tree_sq.remainder_leaves(&root_remainder);
 
     let mut factors: Vec<FactoredModul> = Vec::new();
     // handle each shared factor just once
     let mut shared_factors: HashSet<Integer> = HashSet::new();
+    // reusing the same integer again to avoid assigning new memory - maybe performance boost?
+    let mut g = Integer::new();
+    // reusing constant one for comparing
+    let one = Integer::from(1);
     // zipping zi to zi_squared for factorization
     for (ni, zi_i) in moduli.iter().zip(zi.iter()) {
         let zi_div_ni = zi_i.clone() / ni;
 
-        let g = ni.clone().gcd(&zi_div_ni);
+        g.assign(ni.gcd_ref(&zi_div_ni));
 
-        if g > Integer::from(1) && &g < ni {
+        if g > one && &g < ni {
             let p = g.clone();
-            let q = ni.clone() / &g;
+            // we know division hast rest zero, div_exact is faster
+            let q = ni.clone().div_exact(&g);
             factors.push(to_factored_modul(p, q));
         } else if &g == ni {
             // ni shares both primes with other RSA keys
@@ -73,12 +78,13 @@ fn gernstyle_batch_gcd(moduli: &[Integer]) -> Result<Vec<FactoredModul>> {
     // now treat numbers where both factors are shared with other RSA keys
     'outer: for share in shared_factors {
         for key in moduli {
-            let g = share.clone().gcd(key);
+            g.assign(share.gcd_ref(key));
 
-            if g > Integer::from(1) && g < share {
+            if g > one && g < share {
                 // we found one of the two factors, other by trivial division
-                let other = share.clone() / &g;
-                factors.push(to_factored_modul(g, other));
+                // we know division hast rest zero, div_exact is faster
+                let other = share.clone().div_exact(&g);
+                factors.push(to_factored_modul(g.clone(), other));
                 // continue outer loop as we found the two factos of this key and we do not want duplicates
                 continue 'outer;
             }
@@ -90,58 +96,55 @@ fn gernstyle_batch_gcd(moduli: &[Integer]) -> Result<Vec<FactoredModul>> {
     Ok(factors)
 }
 
-/// input: list of numbers: a, b, c, d
-/// output: product tree of the nums: a, b, c, d, ab, cd, abcd
-fn product_tree(numbers: &[Integer]) -> Result<Vec<Vec<Integer>>> {
-    if numbers.is_empty() {
-        return Err(anyhow!("numbers cannot be empty for product tree"))
-    }
-    let mut levels: Vec<Vec<Integer>> = Vec::new();
-    levels.push(numbers.to_vec());
-
-    // unwrap is safe here, since we added at least one element
-    while levels.last().unwrap().len() > 1 {
-        let prev = levels.last().unwrap();
-        let mut next: Vec<Integer> = Vec::with_capacity((prev.len() + 1) / 2);
-
-        // handle pairs
-        for i in (0..prev.len()).step_by(2) {
-            // check if i+1 exists, otherwise we have a single last element
-            if i + 1 < prev.len() {
-                next.push(prev[i].clone() * prev[i + 1].clone());
-            } else {
-                // last element, nothing to multiply with
-                next.push(prev[i].clone());
-            }
-        }
-        levels.push(next);
-    }
-    Ok(levels)
+// product tree with root at the front for easy position calculation
+struct ProductTree {
+    nodes: Vec<Integer>,
+    leaf_start: usize,
+    leaf_count: usize
 }
 
-fn remainder_tree(sq_tree: &[Vec<Integer>], root: Integer) -> Vec<Integer> {
-    let mut levels: Vec<Vec<Integer>> = Vec::new();
-    levels.push(vec![root]);
+impl ProductTree {
+    fn build(input: &[Integer]) -> Self {
+        let leaf_start = input.len().next_power_of_two();
+        let total_nodes = 2*leaf_start;
 
-    // go from tree top to bottom
-    for level in (1..sq_tree.len()).rev() {
-        // last computed parent of the new children
-        // contains at least one element
-        let parent = levels.last().unwrap();
-        let children = &sq_tree[level - 1];
+        let mut nodes = vec![Integer::from(1); total_nodes];
 
-        let mut remainder: Vec<Integer> = Vec::with_capacity(children.len());
-
-        for (child_id, child_mod) in children.iter().enumerate() {
-            let parent_id = child_id / 2;
-            let remainder_parent = &parent[parent_id];
-            remainder.push(remainder_parent.clone() % child_mod);
+        // put leafs to end of product tree
+        for (i, n) in input.iter().enumerate() {
+            nodes[leaf_start + i].assign(n);
+        }
+        for i in (1..leaf_start).rev() {
+            // we need to split because we want to write in head while reading in tail
+            let (head, tail) = nodes.split_at_mut(2*i);
+            // parent at position i, left at 2*i (0 in tail) and right at 2*i + 1 (1 in tail)
+            head[i].assign(&tail[0] * &tail[1]);
         }
 
-        levels.push(remainder);
+        ProductTree { nodes, leaf_start, leaf_count: input.len() }
     }
 
-    levels.last().unwrap().clone()
+    fn remainder_leaves(&self, root: &Integer) -> Vec<Integer> {
+        let mut remainders = vec![Integer::new(); self.nodes.len()];
+
+        // assign root remainder
+        remainders[1].assign(root);
+
+        for i in 1..self.leaf_start {
+            let (head, tail) = remainders.split_at_mut(2*i);
+            let parent = &head[i];
+
+            // set left node
+            tail[0].assign(parent % &self.nodes[2*i]);
+            // set right 
+            tail[1].assign(parent % &self.nodes[2*i + 1]);
+        }
+        remainders[self.leaf_start .. self.leaf_start + self.leaf_count].to_vec()
+    }
+
+    fn get_root(&self) -> &Integer {
+        &self.nodes[1]
+    }
 }
 
 impl Serialize for FactoredModul {
@@ -157,16 +160,17 @@ impl Serialize for FactoredModul {
 }
 
 #[test]
-fn test_rsa_product_tree() {
-    // test even number of nums
+fn test_new_product_tree() {
     let nums = vec![Integer::from(2), Integer::from(3), Integer::from(4), Integer::from(5)];
-    let expected = vec![nums.clone(), vec![Integer::from(6), Integer::from(20)], vec![Integer::from(120)]];
-    let result = product_tree(&nums).unwrap();
-    assert_eq!(expected, result);
+    let expected = vec![Integer::from(120), Integer::from(6), Integer::from(20)];
+    let result = ProductTree::build(&nums);
+    let branches = &result.nodes[1..result.leaf_start-(result.leaf_count % 2)];
+    assert_eq!(branches, &expected);
 
     let nums = vec![Integer::from(2), Integer::from(3), Integer::from(4), Integer::from(5), Integer::from(6)];
-    let expected = vec![nums.clone(), vec![Integer::from(6), Integer::from(20), Integer::from(6)],
-        vec![Integer::from(120), Integer::from(6)], vec![Integer::from(120*6)]];
-    let result = product_tree(&nums).unwrap();
-    assert_eq!(expected, result);
+    let expected = vec![Integer::from(120*6), Integer::from(120), Integer::from(6), Integer::from(6), Integer::from(20), Integer::from(6)];
+    let result = ProductTree::build(&nums);
+    // substract 1 if leaf_count is odd, as we padded with 1
+    let branches = &result.nodes[1..result.leaf_start-(result.leaf_count % 2)];
+    assert_eq!(branches, &expected);
 }
